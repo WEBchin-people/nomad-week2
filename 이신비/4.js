8dfe6107e703b5c2dcfd682e13e9992cf2b7b843
1eeshinbi <167149442+1eeshinbi@users.noreply.github.com>
2024-05-13 11:22:48 +0900

    Update 4.js

--- 이신비/4.js
+++ 이신비/4.js
@@ -3,11 +3,12 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
+
 function onLoginSubmit(event) {
-  event.preventDefault(); // 새로고침 방지
-  loginForm.classList.add(HIDDEN_CLASSNAME); // form 숨기기
+  event.preventDefault(); 
+  loginForm.classList.add(HIDDEN_CLASSNAME); 
   const username = loginInput.value; 
-  localStorage.setItem(USERNAME_KEY, username); // value 저장
+  localStorage.setItem(USERNAME_KEY, username); 
   paintGreetings(username);
 }
 function paintGreetings(username) {
